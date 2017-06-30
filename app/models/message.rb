0994545c42def6068e940af77a881e8e2a471e21
Elyes <elyes.elbahri77@gmail.com>
2017-06-30 17:14:52 +0200

    some fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
     belongs_to :chat_room
 
     def timestamp
-        created_at.strftime('%H:%M:%S %d %B %Y')
+        created_at.strftime('%d/%m/%Y à %H:%M:%S')
     end
 
     validates :content, presence: true, length: {minimum: 2, maximum: 1000}
